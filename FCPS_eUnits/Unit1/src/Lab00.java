a93ad34eb7f97b2fb356fc649d540cffcdb56b08
CenchEzMoneysniper7 <152701848+CenchEzMoneysniper7@users.noreply.github.com>
2025-02-25 18:17:04 +0800

    Update Lab00.java

--- FCPS_eUnits/Unit1/src/Lab00.java
+++ FCPS_eUnits/Unit1/src/Lab00.java
@@ -11,7 +11,19 @@ public class Lab00 {
 		// TODO Auto-generated method stub
 
 		Display.openWorld("maps/first.map");
+		Display.setSize(10,10);
+		Display.setSpeed(5);
+
 		Robot karel = new Robot();
+		karel.move();
+		karel.pickBeeper();
+		karel.move();
+		karel.turnLeft();
+		karel.move();
+		karel.putBeeper();
+		karel.move();
+		karel.turnLeft();
+		karel.turnLeft();
 	}
 
 }
